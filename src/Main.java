c3a8fa9796dcba807376f02bc63fd02989d4b0af
Дмитрий Когтев <129877863+StrategEqu@users.noreply.github.com>
2023-12-12 15:15:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello, World!");
-            System.out.println("checkout develop");
+        System.out.printf("Hello, World!");
     }
 }
